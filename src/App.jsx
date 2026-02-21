27216bb29d5689270731b23d29120be3d488661b
JesusEscobr <ing.guillermo3627@gmail.com>
2026-02-20 19:24:12 -0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import Header from './components/header'
+import Header from './components/Header'
 import PersonajesGrid from './components/PersonajesGrid'
 
 const App=() =>  {
